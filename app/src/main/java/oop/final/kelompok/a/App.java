9fd9164a6747e4d127d2b07bb27dda7ac97296a9
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2021-11-22 22:56:54 +0800

    add puzzle java file

--- app/src/main/java/oop/final/kelompok/a/App.java
+++ app/src/main/java/oop/final/kelompok/a/App.java
@@ -5,7 +5,7 @@ package oop.final.kelompok.a;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Tampilan awal";
     }
 
     public static void main(String[] args) {
